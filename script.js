0d5e8c5954fc9de140d3e2b0ae9312ce0878a132
sharminishar5 <sharminishar5@gmail.com>
2025-02-17 16:00:02 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Show the popup when the page loads
+window.onload = function() {
+    document.getElementById("birthday-popup").style.display = "flex";
+};
+
+// Function to close the pop-up
+function closePopup() {
+    document.getElementById("birthday-popup").style.display = "none";
+}
